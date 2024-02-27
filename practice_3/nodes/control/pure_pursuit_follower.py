fbc6d92e47c2f2521532959d51dbfdd9efec0187
dmytro <dmytro.zabolotnii@ut.ee>
2024-02-27 15:02:27 +0200

    Practice 3

--- practice_3/nodes/control/pure_pursuit_follower.py
+++ practice_3/nodes/control/pure_pursuit_follower.py
@@ -8,12 +8,14 @@ from tf.transformations import euler_from_quaternion
 
 from shapely.geometry import LineString, Point
 from shapely import prepare, distance
+from scipy.interpolate import interp1d
 
 class PurePursuitFollower:
     def __init__(self):
 
         # Parameters
         self.path_linestring = None
+        self.distance_to_velocity_interpolator = None
         self.lookahead_distance = rospy.get_param("~lookahead_distance")
         self.wheel_base = rospy.get_param("/wheel_base")
         # Publishers
@@ -23,29 +25,40 @@ class PurePursuitFollower:
         rospy.Subscriber('/localization/current_pose', PoseStamped, self.current_pose_callback, queue_size=1)
 
     def path_callback(self, msg):
+        # Transform path to line string
         path_linestring = LineString([(w.pose.pose.position.x, w.pose.pose.position.y) for w in msg.waypoints])
         prepare(path_linestring)
         self.path_linestring = path_linestring
 
+        # Create a distance to velocity interpolator for the path with stopping after reaching final point
+        # collect waypoint x and y coordinates
+        waypoints_xy = np.array([(w.pose.pose.position.x, w.pose.pose.position.y) for w in msg.waypoints])
+        # Calculate distances between points
+        distances = np.cumsum(np.sqrt(np.sum(np.diff(waypoints_xy, axis=0) ** 2, axis=1)))[:-1]
+        # add 0 distance in the beginning and end
+        distances = np.insert(distances, 0, 0)
+        # Extract velocity values at waypoints
+        velocities = np.array([w.twist.twist.linear.x for w in msg.waypoints])[:-1]
+        self.distance_to_velocity_interpolator = interp1d(distances, velocities, kind='linear', bounds_error=False, fill_value=(velocities[0], 0))
+
     def current_pose_callback(self, msg):
-        if self.path_linestring is not None:
+        if self.path_linestring is not None and self.distance_to_velocity_interpolator is not None:
             # Find lookahead distance
             current_pose = Point([msg.pose.position.x, msg.pose.position.y])
             d_ego_from_path_start = self.path_linestring.project(current_pose)
             lookahead_point = self.path_linestring.interpolate(d_ego_from_path_start + self.lookahead_distance)
-            real_lookahead_distance = max(distance(current_pose, lookahead_point), 5)
-            # print('lookahead', real_lookahead_distance)
+            real_lookahead_distance = max(distance(current_pose, lookahead_point), self.lookahead_distance)
             # Find heading and lookahead heading
             _, _, heading = euler_from_quaternion(
                 [msg.pose.orientation.x, msg.pose.orientation.y, msg.pose.orientation.z, msg.pose.orientation.w])
             lookahead_heading = np.arctan2(lookahead_point.y - current_pose.y, lookahead_point.x - current_pose.x)
             # Calculate steering angle
-            steering_angle = np.arctan(2 * self.wheel_base * (lookahead_heading - heading) / real_lookahead_distance)
+            steering_angle = np.arctan(2 * self.wheel_base * np.sin(lookahead_heading - heading) / real_lookahead_distance)
 
             # Publish command
             vehicle_cmd = VehicleCmd()
             vehicle_cmd.ctrl_cmd.steering_angle = steering_angle
-            vehicle_cmd.ctrl_cmd.linear_velocity = 10.0
+            vehicle_cmd.ctrl_cmd.linear_velocity = self.distance_to_velocity_interpolator(d_ego_from_path_start)
             vehicle_cmd.header.frame_id = 'base_link'
             vehicle_cmd.header.stamp = msg.header.stamp
 
