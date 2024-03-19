1b30b10693f105e3c054db8cd41ce18a406b2b2d
dmytro <dmytro.zabolotnii@ut.ee>
2024-03-19 16:15:28 +0200

    Practice 6 almost done

---
+++ practice_6/nodes/localization/localizer.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+
+import math
+import rospy
+
+from tf.transformations import quaternion_from_euler
+from tf2_ros import TransformBroadcaster
+from pyproj import CRS, Transformer, Proj
+
+from novatel_oem7_msgs.msg import INSPVA
+from geometry_msgs.msg import PoseStamped, TwistStamped, Quaternion, TransformStamped, Point
+
+class Localizer:
+    def __init__(self):
+
+        # Parameters
+        self.undulation = rospy.get_param('undulation')
+        utm_origin_lat = rospy.get_param('utm_origin_lat')
+        utm_origin_lon = rospy.get_param('utm_origin_lon')
+
+        # Internal variables
+        self.crs_wgs84 = CRS.from_epsg(4326)
+        self.crs_utm = CRS.from_epsg(25835)
+        self.utm_projection = Proj(self.crs_utm)
+        # Transform origin point
+        self.transformer = Transformer.from_crs(self.crs_wgs84, self.crs_utm)
+        self.origin_x, self.origin_y = self.transformer.transform(utm_origin_lat, utm_origin_lon)
+
+        # Subscribers
+        rospy.Subscriber('/novatel/oem7/inspva', INSPVA, self.transform_coordinates)
+
+        # Publishers
+        self.current_pose_pub = rospy.Publisher('current_pose', PoseStamped, queue_size=10)
+        self.current_velocity_pub = rospy.Publisher('current_velocity', TwistStamped, queue_size=10)
+        self.br = TransformBroadcaster()
+
+    def transform_coordinates(self, msg):
+        # Convert msg coordinates to local coordinates
+        local_x, local_y = self.transformer.transform(msg.latitude, msg.longitude)
+        local_x = local_x - self.origin_x
+        local_y = local_y - self.origin_y
+        local_z = msg.height - self.undulation
+        azimuth_correction = self.utm_projection.get_factors(msg.longitude, msg.latitude).meridian_convergence
+        x, y, z, w = quaternion_from_euler(0, 0, self.convert_azimuth_to_yaw((msg.azimuth - azimuth_correction) * math.pi / 180))
+        local_quaternion = Quaternion(x, y, z, w)
+
+        # Create current pose message
+        current_pose_msg = PoseStamped()
+        current_pose_msg.header.frame_id = 'map'
+        current_pose_msg.header.stamp = msg.header.stamp
+        current_pose_msg.pose.position = Point(local_x, local_y, local_z)
+        current_pose_msg.pose.orientation = local_quaternion
+
+        self.current_pose_pub.publish(current_pose_msg)
+
+        # Create current velocity message
+        current_velocity_msg = TwistStamped()
+        current_velocity_msg.header.frame_id = 'base_link'
+        current_velocity_msg.header.stamp = msg.header.stamp
+        current_velocity_msg.twist.linear.x = (msg.north_velocity ** 2 + msg.east_velocity ** 2) ** 0.5
+
+        self.current_velocity_pub.publish(current_velocity_msg)
+
+        # Create transform message broadcast
+        t = TransformStamped()
+        t.header.frame_id = 'map'
+        t.header.stamp = msg.header.stamp
+        t.child_frame_id = 'base_link'
+        t.transform.translation = current_pose_msg.pose.position
+        t.transform.rotation = local_quaternion
+
+        self.br.sendTransform(t)
+
+    def convert_azimuth_to_yaw(self, azimuth):
+        """
+        Converts azimuth to yaw. Azimuth is CW angle from the North. Yaw is CCW angle from the East.
+        :param azimuth: azimuth in radians
+        :return: yaw in radians
+        """
+        yaw = -azimuth + math.pi / 2
+        # Clamp within 0 to 2 pi
+        if yaw > 2 * math.pi:
+            yaw = yaw - 2 * math.pi
+        elif yaw < 0:
+            yaw += 2 * math.pi
+
+        return yaw
+
+    def run(self):
+        rospy.spin()
+
+
+if __name__ == '__main__':
+    rospy.init_node('localizer')
+    node = Localizer()
+    node.run()
---
+++ practice_6/nodes/planning/local/simple_local_planner.py
@@ -0,0 +1,226 @@
+#!/usr/bin/env python3
+
+import rospy
+import math
+import threading
+from tf2_ros import Buffer, TransformListener, TransformException
+import numpy as np
+from autoware_msgs.msg import Lane, DetectedObjectArray, Waypoint
+from geometry_msgs.msg import PoseStamped, TwistStamped, Vector3, Vector3Stamped
+from shapely.geometry import LineString, Point, Polygon
+from shapely import prepare
+from tf2_geometry_msgs import do_transform_vector3
+from scipy.interpolate import interp1d
+from numpy.lib.recfunctions import unstructured_to_structured
+
+
+class SimpleLocalPlanner:
+    def __init__(self):
+
+        # Parameters
+        self.output_frame = rospy.get_param("~output_frame")
+        self.local_path_length = rospy.get_param("~local_path_length")
+        self.transform_timeout = rospy.get_param("~transform_timeout")
+        self.braking_safety_distance_obstacle = rospy.get_param("~braking_safety_distance_obstacle")
+        self.braking_safety_distance_goal = rospy.get_param("~braking_safety_distance_goal")
+        self.braking_reaction_time = rospy.get_param("braking_reaction_time")
+        self.stopping_lateral_distance = rospy.get_param("stopping_lateral_distance")
+        self.current_pose_to_car_front = rospy.get_param("current_pose_to_car_front")
+        self.default_deceleration = rospy.get_param("default_deceleration")
+
+        # Variables
+        self.lock = threading.Lock()
+        self.global_path_linestring = None
+        self.global_path_distances = None
+        self.distance_to_velocity_interpolator = None
+        self.current_speed = None
+        self.current_position = None
+        self.tf_buffer = Buffer()
+        self.tf_listener = TransformListener(self.tf_buffer)
+
+        # Publishers
+        self.local_path_pub = rospy.Publisher('local_path', Lane, queue_size=1, tcp_nodelay=True)
+
+        # Subscribers
+        rospy.Subscriber('global_path', Lane, self.path_callback, queue_size=None, tcp_nodelay=True)
+        rospy.Subscriber('/localization/current_pose', PoseStamped, self.current_pose_callback, queue_size=1, tcp_nodelay=True)
+        rospy.Subscriber('/localization/current_velocity', TwistStamped, self.current_velocity_callback, queue_size=1, tcp_nodelay=True)
+        rospy.Subscriber('/detection/final_objects', DetectedObjectArray, self.detected_objects_callback, queue_size=1, buff_size=2**20, tcp_nodelay=True)
+
+    def path_callback(self, msg):
+
+        if len(msg.waypoints) == 0:
+            global_path_linestring = None
+            global_path_distances = None
+            distance_to_velocity_interpolator = None
+            rospy.loginfo("%s - Empty global path received", rospy.get_name())
+
+        else:
+            waypoints_xyz = np.array([(w.pose.pose.position.x, w.pose.pose.position.y, w.pose.pose.position.z) for w in msg.waypoints])
+            # convert waypoints to shapely linestring
+            global_path_linestring = LineString(waypoints_xyz)
+            prepare(global_path_linestring)
+
+            # calculate distances between points, use only xy, and insert 0 at start of distances array
+            global_path_distances = np.cumsum(np.sqrt(np.sum(np.diff(waypoints_xyz[:,:2], axis=0)**2, axis=1)))
+            global_path_distances = np.insert(global_path_distances, 0, 0)
+
+            # extract velocity values at waypoints
+            velocities = np.array([w.twist.twist.linear.x for w in msg.waypoints])
+            # create interpolator
+            distance_to_velocity_interpolator = interp1d(global_path_distances, velocities, kind='linear', bounds_error=False, fill_value=0.0)
+
+            rospy.loginfo("%s - Global path received with %i waypoints", rospy.get_name(), len(msg.waypoints))
+
+        with self.lock:
+            self.global_path_linestring = global_path_linestring
+            self.global_path_distances = global_path_distances
+            self.distance_to_velocity_interpolator = distance_to_velocity_interpolator
+
+    def current_velocity_callback(self, msg):
+        # save current velocity
+        self.current_speed = msg.twist.linear.x
+
+    def current_pose_callback(self, msg):
+        # save current pose
+        current_position = Point([msg.pose.position.x, msg.pose.position.y])
+        self.current_position = current_position
+
+    def detected_objects_callback(self, msg):
+        if self.global_path_linestring is None or self.current_position is None or self.current_speed is None:
+            self.publish_local_path_wp([], msg.header.stamp, msg.header.frame_id)
+            return
+        # Copy variables for callback
+        with self.lock:
+            global_path_linestring = self.global_path_linestring
+            global_path_distances = self.global_path_distances
+            distance_to_velocity_interpolator = self.distance_to_velocity_interpolator
+            current_position = self.current_position
+        # Extract local path
+        d_ego_from_path_start = global_path_linestring.project(current_position)
+        local_path = self.extract_local_path(global_path_linestring, global_path_distances, d_ego_from_path_start, self.local_path_length)
+
+        if local_path is None:
+            self.publish_local_path_wp([], msg.header.stamp, msg.header.frame_id)
+            return
+        prepare(local_path)
+        # Local path buffer
+        local_path_buffer = local_path.buffer(self.stopping_lateral_distance, cap_style="flat")
+        prepare(local_path_buffer)
+        # Prepare transform to base link
+        transform = self.tf_buffer.lookup_transform("base_link", msg.header.frame_id, msg.header.stamp,
+                                                    rospy.Duration(self.transform_timeout))
+        # Intersection and object distances calculation
+        objects_distances_from_path_start = []
+        object_velocities = []
+        is_obstacle_array = []
+        for detectedobject in msg.objects:
+            convex_hull_polygon = Polygon([[point.x, point.y] for point in detectedobject.convex_hull.polygon.points])
+            if local_path_buffer.intersects(convex_hull_polygon):
+                intersection = local_path_buffer.intersection(convex_hull_polygon)
+                objects_distances_from_path_start.append(min([local_path.project(Point(coords))
+                                                         for coords in intersection.exterior.coords]))
+
+                # project object velocity to base_link frame to get longitudinal speed
+                # in case there is no transform assume the object is not moving
+                if transform is not None:
+                    vector3_stamped = Vector3Stamped(vector=detectedobject.velocity.linear)
+                    velocity = do_transform_vector3(vector3_stamped, transform).vector
+                else:
+                    velocity = Vector3()
+
+                object_velocities.append(velocity.x)
+                is_obstacle_array.append(1)
+        # Stop point adding
+        if local_path_buffer.intersects(Point(global_path_linestring.coords[-1])):
+            objects_distances_from_path_start.append(local_path.project(Point(global_path_linestring.coords[-1])))
+            object_velocities.append(0)
+            is_obstacle_array.append(0)
+        # Numpifying
+        objects_distances_from_path_start = np.array(objects_distances_from_path_start)
+        object_velocities = np.array(object_velocities)
+        is_obstacle_array = np.array(is_obstacle_array)
+        # Target speed calculations
+        if len(objects_distances_from_path_start) > 0:
+            object_braking_distances = np.where(is_obstacle_array, self.braking_safety_distance_obstacle, self.braking_safety_distance_goal)
+            target_distances = (objects_distances_from_path_start - self.current_pose_to_car_front -
+                                self.braking_reaction_time * np.abs(object_velocities) - object_braking_distances)
+
+            calculated_target_velocities = np.maximum((np.maximum(object_velocities, 0) ** 2 + 2 * self.default_deceleration *
+                                                      target_distances), 0) ** 0.5
+
+            closest_object_distance = objects_distances_from_path_start[np.argmin(calculated_target_velocities)] - self.current_pose_to_car_front
+            closest_object_velocity = object_velocities[np.argmin(calculated_target_velocities)]
+            closest_object_braking = object_braking_distances[np.argmin(calculated_target_velocities)]
+            stopping_point_distance = (objects_distances_from_path_start[np.argmin(calculated_target_velocities)] -
+                                       self.braking_reaction_time * closest_object_velocity - closest_object_braking)
+
+            target_velocity = min(distance_to_velocity_interpolator(d_ego_from_path_start), np.min(calculated_target_velocities))
+            local_path_blocked = np.max(is_obstacle_array) > 0
+        else:
+            closest_object_distance = 0
+            closest_object_velocity = 0
+            stopping_point_distance = 0
+            target_velocity = distance_to_velocity_interpolator(d_ego_from_path_start)
+            local_path_blocked = False
+
+        # Waypoints
+        local_path_waypoints = self.convert_local_path_to_waypoints(local_path, target_velocity)
+        # Publish waypoints
+        self.publish_local_path_wp(local_path_waypoints, msg.header.stamp, msg.header.frame_id, closest_object_distance,
+                                                                closest_object_velocity, local_path_blocked, stopping_point_distance)
+
+    def extract_local_path(self, global_path_linestring, global_path_distances, d_ego_from_path_start, local_path_length):
+
+        # current position is projected at the end of the global path - goal reached
+        if math.isclose(d_ego_from_path_start, global_path_linestring.length):
+            return None
+
+        d_to_local_path_end = d_ego_from_path_start + local_path_length
+
+        # find index where distances are higher than ego_d_on_global_path
+        index_start = np.argmax(global_path_distances >= d_ego_from_path_start)
+        index_end = np.argmax(global_path_distances >= d_to_local_path_end)
+
+        # if end point of local_path is past the end of the global path (returns 0) then take index of last point
+        if index_end == 0:
+            index_end = len(global_path_linestring.coords) - 1
+
+        # create local path from global path add interpolated points at start and end, use sliced point coordinates in between
+        start_point = global_path_linestring.interpolate(d_ego_from_path_start)
+        end_point = global_path_linestring.interpolate(d_to_local_path_end)
+        local_path = LineString([start_point] + list(global_path_linestring.coords[index_start:index_end]) + [end_point])
+
+        return local_path
+
+    def convert_local_path_to_waypoints(self, local_path, target_velocity):
+        # convert local path to waypoints
+        local_path_waypoints = []
+        for point in local_path.coords:
+            waypoint = Waypoint()
+            waypoint.pose.pose.position.x = point[0]
+            waypoint.pose.pose.position.y = point[1]
+            waypoint.pose.pose.position.z = point[2]
+            waypoint.twist.twist.linear.x = target_velocity
+            local_path_waypoints.append(waypoint)
+        return local_path_waypoints
+
+    def publish_local_path_wp(self, local_path_waypoints, stamp, output_frame, closest_object_distance=0.0, closest_object_velocity=0.0, local_path_blocked=False, stopping_point_distance=0.0):
+        # create lane message
+        lane = Lane()
+        lane.header.frame_id = output_frame
+        lane.header.stamp = stamp
+        lane.waypoints = local_path_waypoints
+        lane.closest_object_distance = closest_object_distance
+        lane.closest_object_velocity = closest_object_velocity
+        lane.is_blocked = local_path_blocked
+        lane.cost = stopping_point_distance
+        self.local_path_pub.publish(lane)
+
+    def run(self):
+        rospy.spin()
+
+if __name__ == '__main__':
+    rospy.init_node('simple_local_planner')
+    node = SimpleLocalPlanner()
+    node.run()
